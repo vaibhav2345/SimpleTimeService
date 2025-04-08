0e28055d0794dfe49ab63c0e7d096c5dec4e4531
EC2 Default User <ec2-user@ip-172-31-7-1.ap-south-1.compute.internal>
2025-04-08 06:33:17 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, jsonify, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route('/')
+def simple_time_service():
+    return jsonify({
+        "timestamp": datetime.utcnow().isoformat(),
+        "ip": request.remote_addr
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
